512a1d440a82369f6b6580e5dfaba3ec12832339
haytna <maaa.alluh@gmail.com>
2018-06-01 06:07:40 +0200

    Add files via upload

---
+++ enter your country.py
@@ -0,0 +1,146 @@
+
+1) Enter name of your country or any country you like!
+2) Suggestions are always welcome????
+"""
+
+#*********************************
+#***Updates - More countries added
+#***    
+#***No need to capatalise the fist ****letter, now input can be-
+#*** India or india or InDia etc.
+
+#***(Special thanks to all of them ****who check my code and Elliot ****Stenquist and many others...)                       ****????????????????????????????
+#*********************************
+
+
+#New update - More than 105 countries added!!!
+# If your country do not found, then please let me know in comment box!
+
+x = input().lower()
+
+
+if x == "india" or x=="mauritius" or x=="fiji":
+    print ("Hindi : Mujhe coding karna bahut accha lagta hai!")
+
+
+elif x == "nepal":
+    print ("Nepali : Malai coding garna manparxa!")
+
+elif x == "france" or x=="benin" or x=="burkina" or x== "faso" or x== "burundi" or x=="cameroon" or x=="chad" or x=="the ivory coast" or x=="congo" or x=="djibouti" or x== "equatorial guinea" or x=="haiti" or x=="luxembourg" or x=="madagascar" or x=="mali" or x=="monaco" or x=="niger" or x=="rwanda" or x=="senegal" or x=="seychelles" or x=="togo" or x=="vanuatu":
+    print ("French : J'aime coder!")
+
+
+elif x=="italy" or x=="sardinia" or x=="sicily" or x=="san marino" or x=="vatican":
+    print ("Italian : Amo la programmazione!")
+
+
+elif x == "netherlands":
+    print ("Dutch : Ik hou van programmeren")
+    
+    
+elif x=="romania":
+    print ("Romanian : Imi place codarea")
+
+
+elif x == "germany" or x=="austria" or x=="luxembourg" or x == "liechtenstein":
+    print ("German : Ich liebe es zu programmieren!")
+    
+    
+elif x == "scotland":
+    print ("Scots: A love tae code")
+
+
+elif x == "spain" or x=="argentina" or x=="bolivia" or x=="chile" or x== "colombia" or x=="costa rica" or x== "cuba" or x=="dominican republic" or x== "ecuador" or x=="el salvador" or x== "equatorial guinea" or x=="guatemala" or x=="honduras" or x== "mexico" or x=="nicaragua" or x=="panama" or x=="paraguay" or x=="peru" or x=="uruguay" or x== "venezuela":
+    print("Spanish : Me encanta la codificaci?n!")
+
+
+elif x == "portugal" or x=="brazil" or x== "mozambique" or x == "angola" or x== "guinea-bissau" or x == "macau" or x=="cape verde": 
+    print ("Portuguese : Eu amo codificar!")
+
+elif x == "indonesia":
+    print ("Indonesian : Saya suka coding!")
+
+elif x=="china" or x=="hong kong" or x== "malaysia" or x=="taiwan":
+    print ("Chinese : Wo xihuan bianma")
+    
+    
+elif x=="singapore" :
+    print ("English : I love coding\nTamil : Nan kuriyittu necikkiren\nMalay : Saya suka coding\nChinese : Wo xihuan bianma")
+
+
+elif x=="philippines":
+    print ("English : I love coding\nFilipino : Gustung-gusto ko ang coding")
+
+    
+elif x == "poland":
+    print ("Polish : Uwielbiam kodowa?")
+
+
+elif x == "slovakia":
+    print ("Slovak : Milujem kodovanie")
+
+
+elif x=="algeria" or x =="chad" or x=="egypt" or x=="iraq" or x=="israel" or x=="jordan" or x == "kuwait" or x=="libya" or x=="oman" or x=="qatar" or x=="saudi arabia" or x=="syria" or x =="uae":
+    print ("Arabic : 'ana 'uhibu altarmiz")
+    
+
+elif x=="iran":
+    print ("Persian : Man codnevisi ro dost daram")
+    
+    
+elif x== "finland":
+    print ("Finnish : Rakastan koodia!")
+    
+    
+elif x=="japan":
+    print ("Japanese : Watashi wa kodingu ga daisuki")
+    
+    
+elif x == "sweden":
+    print ("Swedish : Jag alskar kodning")
+    
+    
+elif x == "russia":
+    print ("Russian : Mne nravitsya kodirovaniye")
+
+
+elif x == "mongolia":
+    print ("Mongolian : Bi kod bichikhdee durtai")
+    
+    
+elif x == "hungary":
+    print ("Hungarian : Imadom a kodolast!")
+    
+
+elif x == "thailand":
+    print ("Thai :Shan rak coding!")
+
+    
+elif x == "turkey":
+    print("Turkish : Kodlamayi seviyorum")
+
+
+elif x == "canada":
+    print ("English : I love coding\nFrench : J'aime coder")
+    
+    
+elif x =="switzerland":
+    print ("French : J'aime coder\nGerman : Ich liebe es zu programmieren\nItalian : Amo la programmazione")
+
+
+elif x == "usa" or x=="britain" or x=="england" or x =="australia" or x=="ireland" or x =="jamaica" or x=="new zealand" or x=="nigeria" or x =="united states" :
+    print ("English : I love code!")
+
+
+elif x == "belgium":
+    print ("Dutch : Ik hou van coderen\nFrench : J'aime coder\nGerman : Ich liebe es zu programmieren")
+
+
+elif x == "bangladesh":
+    print ("Bengali : Ami kodim bhalabasi")
+
+
+else :
+    print ("Sorry your country is not found,\nI might forgot to add your country in list, (if so please tell me comment box)")
+
+print ("\nHope you like it\nSuggestions are always welcome :D\nI know there might be many simpler way to do this but I realised it when I completed this code :)")
